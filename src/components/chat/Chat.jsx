ddd3b0c252901cb801100e16e606cce0d3872143
ciavash <svsh.kian@gmail.com>
2024-07-08 08:32:14 -0700

    developing top section of the chat component

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
-import "./chat.css"
+import React from "react";
+import "./chat.css";
 function Chat() {
   return (
-    <div className='chat'>
-      
+    <div className="chat">
+      <div className="top">
+        <div className="user">
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <span className="">mohammad ali</span>
+            <p>
+              Lorem ipsum dolor sit amet.
+            </p>
+          </div>
+        </div>
+        <div className="icons">
+          <img src="./phone.png" alt="phonecall" />
+          <img src="./video.png" alt="videocall" />
+          <img src="./info.png" alt="info" />
+        </div>
+      </div>
+      <div className="center"></div>
+      <div className="bottom"></div>
     </div>
-  )
+  );
 }
 
-export default Chat
+export default Chat;
