4c05cee589c82fe8634196c4253f7876ba7e99da
ciavash <svsh.kian@gmail.com>
2024-07-09 11:53:14 -0700

    developing the login component

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
-
+import React, { useState } from "react";
+import "./Login.css";
 function Login() {
+  const [avatar, setAvatar] = useState({
+    file: null,
+    url: "",
+  });
+  const handleAvatar = (e) => {
+    if (e.target.files[0]) {
+      setAvatar({
+        file: e.target.files[0],
+        url: URL.createObjectURL(e.target.files[0]),
+      });
+    }
+  };
   return (
-    <div>Login</div>
-  )
+    <div className="login">
+      <div className="item">
+        <form>
+          <input type="text" placeholder="Email" name="email" />
+          <input type="password" placeholder="Password" name="password" />
+          <button>Sign in</button>
+        </form>
+      </div>
+      <div className="seperator"></div>
+      <div className="item">
+        <form>
+          <label style={{ cursor: "pointer" }} htmlFor="file">
+            <img src={avatar.url || "./avatar.png"} />
+            Upload an Image
+          </label>
+          <input
+            id="file"
+            type="file"
+            style={{ display: "none" }}
+            onChange={handleAvatar}
+          />
+          <input type="text" placeholder="Username" name="username" />
+          <input type="text" placeholder="Email" name="email" />
+          <input type="password" placeholder="Password" name="password" />
+          <button>Sign up</button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
