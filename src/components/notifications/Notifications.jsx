04502372b8d73bd431cf71566d0370dd2fabfe16
ciavash <svsh.kian@gmail.com>
2024-07-09 12:15:49 -0700

    creating notification component

---
+++ src/components/notifications/Notifications.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { ToastContainer } from "react-toastify";
+function Notifications() {
+  return (
+    <div>
+      <ToastContainer position="bottom-right"/>
+    </div>
+  );
+}
+
+export default Notifications;
