530e06c19f38fe18dbfd97820e59b99e81d3fa35
ciavash <svsh.kian@gmail.com>
2024-07-10 12:39:13 -0700

    creating firebase storage

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,8 +1,9 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import{getAuth} from "firebase/auth"
+import { getAuth } from "firebase/auth";
 import { Firestore, getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_API_KEY,
   authDomain: "chatapp-7b9dd.firebaseapp.com",
@@ -17,4 +18,5 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 export const auth = () => getAuth(app);
-export const db  = () => getFirestore(app);
+export const db = () => getFirestore(app);
+export const storage = () => getStoragegit(app);
