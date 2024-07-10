56e788bdc3382759def0ea6d7de3514daa008335
ciavash <svsh.kian@gmail.com>
2024-07-10 12:37:06 -0700

    creating firebase db

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,7 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-
+import{getAuth} from "firebase/auth"
+import { Firestore, getFirestore } from "firebase/firestore";
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_API_KEY,
   authDomain: "chatapp-7b9dd.firebaseapp.com",
@@ -15,3 +16,5 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+export const auth = () => getAuth(app);
+export const db  = () => getFirestore(app);
